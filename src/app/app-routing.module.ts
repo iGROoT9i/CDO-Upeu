8db5a70a2267105b42a58d9d6e5ead0efa948636
GROoT <jhonluna@upeu.edu.pe>
2021-07-11 20:21:51 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }, { path: 'inicio', loadChildren: () => import('./inicio/inicio.module').then(m => m.InicioModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
